0fb4f9797e2776c61d14085640b4d9216cd2a121
Easwaramurthy <easwarp45@gmail.com>
2026-04-15 20:17:52 +0530

    version v1.0.1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,11 +81,11 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "url": "registerSW.js",
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
-    "url": "/index.html",
-    "revision": "0.t67f7an4gmo"
+    "url": "/offline.html",
+    "revision": "0.c8jq12uqdpg"
   }], {});
   workbox.cleanupOutdatedCaches();
-  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
     allowlist: [/^\/$/],
     denylist: [/^\/api/]
   }));
